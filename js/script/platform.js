497876bfbaa0d0b96dc9d9a4b3b7811674a88014
Sue Roh <sr3022@columbia.edu>
2016-10-28 19:42:58 -0400

    Added second cube to platform

--- js/script/platform.js
+++ js/script/platform.js
@@ -1,12 +1,22 @@
-var geometry = new THREE.BoxGeometry(600, 600,600);
-var texture = new THREE.TextureLoader().load('textures/pink-gradient.jpg');
-texture.wrapS = THREE.RepeatWrapping;
-texture.wrapT = THREE.RepeatWrapping;
-texture.repeat.set( 4, 4 );
+var geometry = new THREE.BoxGeometry(500, 500, 500);
 
+// TEXTURES:
+var texture = new THREE.TextureLoader().load('textures/pink-gradient.jpg');
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set( 4, 4 );
 var material = new THREE.MeshBasicMaterial({ map: texture });
 
+// CUBE1:
 var cube = new THREE.Mesh(geometry, material);
-
+    cube.position.x = 100;
+    cube.position.y = 500;
+    cube.position.z = 600;
 scene.add(cube);
 
+// CUBE2:
+var cube2 = new THREE.Mesh(geometry, material);
+    cube.position.x = 300;
+    cube.position.y = 200;
+    cube.position.z = 900;
+scene.add(cube2);
